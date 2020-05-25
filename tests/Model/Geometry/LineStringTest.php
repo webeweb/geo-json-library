7e9d1c6a4be2f77f014b9bb6790fac822befa4db
webeweb <support@webeweb.fr>
2020-05-25 19:04:29 +0200

    Add Line string model

---
+++ tests/Model/Geometry/LineStringTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the geo-json-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoJSON\Tests\Model\Geometry;
+
+use WBW\Library\GeoJSON\Tests\AbstractTestCase;
+use WBW\Library\GeoJSON\Model\GeoJson;
+use WBW\Library\GeoJSON\Model\Geometry\LineString;
+
+/**
+ * Line string test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GeoJSON\Tests\Model\Geometry
+ */
+class LineStringTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     */
+    public function test__construct() {
+
+        $obj = new LineString();
+
+        $this->assertEquals(GeoJson::TYPE_LINESTRING, $obj->getType());
+    }
+}
