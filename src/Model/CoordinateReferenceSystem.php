28753522c89af7f3061abaecc5caf32088b9de39
webeweb <support@webeweb.fr>
2020-05-26 18:30:30 +0200

    Add abstract Coordinate reference system

---
+++ src/Model/CoordinateReferenceSystem.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the geo-json-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoJSON\Model;
+
+/**
+ * Coordinate reference system.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GeoJSON\Model
+ * @abstract
+ */
+abstract class CoordinateReferenceSystem extends GeoJson {
+
+}
