51044c22eff3ca0343aed814c8672290199fa0c8
webeweb <support@webeweb.fr>
2020-05-26 12:39:15 +0200

    Add Position model

---
+++ src/Model/Position.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the geo-json-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoJSON\Model;
+
+/**
+ * Position.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GeoJSON\Model
+ */
+class Position {
+
+    /**
+     * Altitude.
+     *
+     * @var float
+     */
+    private $altitude;
+
+    /**
+     * Latitude.
+     *
+     * @var float
+     */
+    private $latitude;
+
+    /**
+     * Longitude.
+     *
+     * @var float
+     */
+    private $longitude;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Get the altitude.
+     *
+     * @return float Returns the altitude.
+     */
+    public function getAltitude() {
+        return $this->altitude;
+    }
+
+    /**
+     * Get the latitude.
+     *
+     * @return float Returns the latitude.
+     */
+    public function getLatitude() {
+        return $this->latitude;
+    }
+
+    /**
+     * Get the longitude.
+     *
+     * @return float Returns the longitude.
+     */
+    public function getLongitude() {
+        return $this->longitude;
+    }
+
+    /**
+     * Set the altitude.
+     *
+     * @param float $altitude The altitude.
+     * @return Position Returns this position.
+     */
+    public function setAltitude($altitude) {
+        $this->altitude = $altitude;
+        return $this;
+    }
+
+    /**
+     * Set the latitude.
+     *
+     * @param float $latitude The latitude.
+     * @return Position Returns this position.
+     */
+    public function setLatitude($latitude) {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    /**
+     * Set the longitude.
+     *
+     * @param float $longitude The longitude.
+     * @return Position Returns this position.
+     */
+    public function setLongitude($longitude) {
+        $this->longitude = $longitude;
+        return $this;
+    }
+}
---
+++ tests/Model/PositionTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the geo-json-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoJSON\Tests\Model;
+
+use WBW\Library\GeoJSON\Model\Position;
+use WBW\Library\GeoJSON\Tests\AbstractTestCase;
+
+/**
+ * Position test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GeoJSON\Tests\Model
+ */
+class PositionTest extends AbstractTestCase {
+
+    /**
+     * Tests the setAltitude() method.
+     *
+     * @return void
+     */
+    public function testSetAltitude() {
+
+        $obj = new Position();
+
+        $obj->setAltitude(0.123456789);
+        $this->assertEquals(0.123456789, $obj->getAltitude());
+    }
+
+    /**
+     * Tests the setLatitude() method.
+     *
+     * @return void
+     */
+    public function testSetLatitude() {
+
+        $obj = new Position();
+
+        $obj->setLatitude(0.123456789);
+        $this->assertEquals(0.123456789, $obj->getLatitude());
+    }
+
+    /**
+     * Tests the setLongitude() method.
+     *
+     * @return void
+     */
+    public function testSetLongitude() {
+
+        $obj = new Position();
+
+        $obj->setLongitude(0.123456789);
+        $this->assertEquals(0.123456789, $obj->getLongitude());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $obj = new Position();
+
+        $this->assertNull($obj->getLongitude());
+        $this->assertNull($obj->getLatitude());
+        $this->assertNull($obj->getAltitude());
+    }
+}
