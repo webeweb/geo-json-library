f0757764a413c262da568c180e35a1bc8743dc4d
webeweb <support@webeweb.fr>
2023-02-24 09:18:43 +0100

    Add Serializer keys interface

---
+++ src/Serializer/SerializerKeys.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the geo-json-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoJson\Serializer;
+
+/**
+ * Serializer keys.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GeoJson\Serializer
+ */
+class SerializerKeys {
+
+    /**
+     * Serializer key "bbox".
+     *
+     * @var string
+     */
+    const BBOX = "bbox";
+
+    /**
+     * Serializer key "coordinates".
+     *
+     * @var string
+     */
+    const COORDINATES = "coordinates";
+
+    /**
+     * Serializer key "features".
+     *
+     * @var string
+     */
+    const FEATURES = "features";
+
+    /**
+     * Serializer key "geometries".
+     *
+     * @var string
+     */
+    const GEOMETRIES = "geometries";
+
+    /**
+     * Serializer key "geometry".
+     *
+     * @var string
+     */
+    const GEOMETRY = "geometry";
+
+    /**
+     * Serializer key "properties".
+     *
+     * @var string
+     */
+    const PROPERTIES = "properties";
+}
---
+++ tests/Serializer/SerializerKeysTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the geo-json-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoJson\Tests\Serializer;
+
+use WBW\Library\GeoJson\Serializer\SerializerKeys;
+use WBW\Library\GeoJson\Tests\AbstractTestCase;
+
+/**
+ * Serializer keys test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GeoJson\Tests\Serializer
+ */
+class SerializerKeysTest extends AbstractTestCase {
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("bbox", SerializerKeys::BBOX);
+        $this->assertEquals("coordinates", SerializerKeys::COORDINATES);
+        $this->assertEquals("features", SerializerKeys::FEATURES);
+        $this->assertEquals("geometries", SerializerKeys::GEOMETRIES);
+        $this->assertEquals("geometry", SerializerKeys::GEOMETRY);
+        $this->assertEquals("properties", SerializerKeys::PROPERTIES);
+    }
+}
